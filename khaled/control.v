2a459bf053a89e67d7eb730d6c3da334dac8145e
KhaledAtefMansour <44950887+KhaledAtefMansour@users.noreply.github.com>
2019-10-17 13:32:20 +0200

    Update control.v

--- khaled/control.v
+++ khaled/control.v
@@ -1,54 +1,39 @@
+`timescale 1ns / 1ps
 module control(RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,ALUOp,Instruction,jump);
+
 input [5:0] Instruction;
 output reg ALUSrc,RegWrite,MemRead,MemWrite,Branch,jump;
 output reg [1:0] ALUOp,RegDst,MemtoReg;
 
-always
+always @*
 
-case(Instruction)
-0:// R-type Instructions
-begin
-RegDst<=1;ALUSrc<=0;MemtoReg<=0;RegWrite<=1;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=2;jump<=0;
+casex(Instruction)
+0:begin RegDst<=1;ALUSrc<=0;MemtoReg<=0;RegWrite<=1;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=2;jump<=0; // R-type Instructions
 end
 
-35:	// load-word Instruction
-begin
-RegDst<=0;ALUSrc<=1;MemtoReg<=1;RegWrite<=1;MemRead<=1;MemWrite<=0;Branch<=0;ALUOp<=0;jump<=0;
+35:begin RegDst<=0;ALUSrc<=1;MemtoReg<=1;RegWrite<=1;MemRead<=1;MemWrite<=0;Branch<=0;ALUOp<=0;jump<=0; 	// load-word Instruction
 end
 
-43:	// store-word Instruction
-begin
-RegDst<=2'bx;ALUSrc<=1;MemtoReg<=2'bx;RegWrite<=0;MemRead<=0;MemWrite<=1;Branch<=0;ALUOp<=0;jump<=0;
+43:begin RegDst<=2'bxx;ALUSrc<=1;MemtoReg<=2'bxx;RegWrite<=0;MemRead<=0;MemWrite<=1;Branch<=0;ALUOp<=0;jump<=0; // store-word Instruction
 end
 
-4: 	// Branch-equal Instruction
-begin
-RegDst<=2'bx;ALUSrc<=0;MemtoReg<=2'bx;RegWrite<=0;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=1;jump<=0;
+4:begin RegDst<=2'bxx;ALUSrc<=0;MemtoReg<=2'bxx;RegWrite<=0;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=1;jump<=0; // Branch-equal Instruction
 end
 
-8:	// add-imediate Instruction
-begin
-RegDst<=0;ALUSrc<=1;MemtoReg<=0;RegWrite<=1;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=0;jump<=0;
+8:begin RegDst<=0;ALUSrc<=1;MemtoReg<=0;RegWrite<=1;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=0;jump<=0; // add-imediate Instruction
 end
 
-13:	// or-imediate Instruction
-begin
-RegDst<=0;ALUSrc<=1;MemtoReg<=0;RegWrite<=1;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=3;jump<=0;
+13:begin RegDst<=0;ALUSrc<=1;MemtoReg<=0;RegWrite<=1;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=3;jump<=0; // or-imediate Instruction
 end
 
-2:	// jump Instruction
-begin
-RegDst<=2'bx;ALUSrc<=1'bx;MemtoReg<=2'bx;RegWrite<=0;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=2'bx;jump<=1;
+2:begin RegDst<=2'bxx;ALUSrc<=1'bx;MemtoReg<=2'bxx;RegWrite<=0;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=2'bxx;jump<=1; // jump Instruction
 end
 
-3: // jump and link instruction
-begin
-RegDst<=2;ALUSrc<=1'bx;MemtoReg<=2;RegWrite<=1;MemRead<=0;MemWrite<=0;Branch<=1'bx;ALUOp<=2'bx;jump<=1;
+3:begin RegDst<=2;ALUSrc<=1'bx;MemtoReg<=2;RegWrite<=1;MemRead<=0;MemWrite<=0;Branch<=1'bx;ALUOp<=2'bxx;jump<=1; // jump and link instruction
 end
 
-default
-begin
-RegDst<=2'bx;ALUSrc<=1'bx;MemtoReg<=1=2'bx;RegWrite<=0;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=2'bx;jump<=0;
+default:begin RegDst<=0;ALUSrc<=1'bx;MemtoReg<=0;RegWrite<=0;MemRead<=0;MemWrite<=0;Branch<=0;ALUOp<=0;jump<=0;
 end
 
+endcase
 endmodule
