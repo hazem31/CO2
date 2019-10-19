3aab445db0a86bea88a7f3405e9d1c3cce4bf12d
KhaledAtefMansour <44950887+KhaledAtefMansour@users.noreply.github.com>
2019-10-19 23:06:59 +0200

    Update CPU.v

--- khaled/CPU.v
+++ khaled/CPU.v
@@ -350,5 +350,74 @@ case(SEL)
 endcase
 
 endmodule
+`timescale 1 ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:32:39 10/19/2019 
+// Design Name: 
+// Module Name:    MIPS_CPU 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module MIPS_CPU();
+
+reg [31:0] PC;
+wire [31:0] IR,MemToRegOut,ReadData1,ReadData2,SignEE,ALUin2,AluOutput,Data,shiftedSignEE,branch_muxOut,jump_addresss,JumpOut;// check with TA (IR)
+wire RegWriteOut,RegWrite,CLK,ALUSrc,JRMuxControl,ZeroFlag,MemWrite,MemRead,Branch,jump,isBranch;
+wire [1:0] RegDst,MemtoReg; // 2 bits for JAL 
+wire [2:0] AluOp;
+wire[3:0] AluControl;
+wire [4:0] RegDstOut;
+
+assign RegWriteOut = RegWrite & (~JRMuxControl);
+assign SignEE = { {16{IR[15]}}, IR[15:0] };
+assign shiftedSignEE = (SignEE <<2) + PC;
+assign isBranch = Branch & ZeroFlag;
+assign jump_addresss = { {PC[31:28]}, (IR[27:0]<<2) };
+
+always @(posedge CLK)
+begin
+PC = PC + 4;
+end
+
+Clock_Generator clock (CLK);
+
+InstructionMemory IM(PC>>2,IR);//check "khaled"
+
+MUX4input RegDstMux(IR[20:16],IR[15:11],31,0,RegDstOut,RegDst);// check with TA inputs problem
+
+Register_File RF(IR[25:21],IR[20:16],RegDstOut,MemToRegOut,RegWriteOut,ReadData1,ReadData2,CLK);
+
+MUX ALU_SRC (ReadData2,SignEE,ALUin2,ALUSrc);
+
+AluControlUint ALUcontrolunit1(AluControl,JRMuxControl,IR[5:0],AluOp);
+
+AluMips ALU(AluOutput,ZeroFlag,ReadData1,ALUin2,AluControl,IR[10:6]);
+
+DataMemory Data_memory(Data,CLK,AluOutput,MemWrite,MemRead,ReadData2);
+
+MUX4input MemtoRegMUX(AluOutput,Data,PC,0,MemToRegOut,MemtoReg);
+
+control Control_Unit(RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,AluOp,IR[31:26],jump);
+
+MUX BranchMUX (PC,shiftedSignEE,branch_muxOut,isBranch);
+
+MUX JumpMUX (branch_muxOut,jump_addresss,JumpOut,jump);
+
+MUX JumpRegMUX(JumpOut,ReadData1,PC,JRMuxControl);
+
+
+endmodule
 
 
